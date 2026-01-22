d14c19db3dd8deb88628aabd64c155fd875733a4
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-22 15:25:44 +0100

    Aggiunta lista di articoli con gestione dello stato tramite useState

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react"
+
+//array da usare come valore iniziale della variabile di stato dei dati 
+const initialArticles = [
+    'Introduzione allo sviluppo web',
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Frontend e Backend',
+    'Git e Github',
+    'Best practies'];
+
+
 function ArticleList() {
+
+    // creiamo la var di stato per gestire gli articoli
+    const [articles, setArticles] = useState(initialArticles);
 
     return (
         <>
-            <h1>Lista di articoli</h1>
-           
+            <div>
+                <h1>Lista di articoli</h1>
+                <ul>
+                    {
+                        articles.map((article, index) => (
+                            <li key={index}>
+                                {article}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </>
     )
 }
